be38ca15499dda5f809b222d9563403b4511f8a0
rathishvarathan <86611355+rathishvarathan@users.noreply.github.com>
2021-08-15 08:38:17 -0700

    Add files via upload

---
+++ app_comstants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:hexcolor/hexcolor.dart';
+
+
+
+final Color mainHexColor = HexColor("#1C1C1C");
+final Color accentHexColor = HexColor("#FCC91C");
